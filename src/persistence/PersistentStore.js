d41baa48fe8af9ff18bee1246528f6387d909f8b
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2016-09-16 13:39:48 +0200

    Removed warnings

--- src/persistence/PersistentStore.js
+++ src/persistence/PersistentStore.js
@@ -5,18 +5,18 @@ import initialState from '../model/State';
 import { persistData, retriveData } from '../persistence/Persistence';
 
 const initialize = initialState => {
-  console.log('retrieving data from cache with fallback:', initialState);
+  console.log('retrieving data from cache with fallback:', initialState); // eslint-disable-line no-console
   const state = retriveData(initialState);
-  console.log('state resolved to: ', state);
+  console.log('state resolved to: ', state); // eslint-disable-line no-console
   return state;
 };
 
 const middleware = store => next => action => {
-  console.log('dispatching', action);
+  console.log('dispatching', action); // eslint-disable-line no-console
   const result = next(action);
   const state = store.getState();
   persistData(state);
-  console.log('next state', state);
+  console.log('next state', state); // eslint-disable-line no-console
   return result;
 };
 
